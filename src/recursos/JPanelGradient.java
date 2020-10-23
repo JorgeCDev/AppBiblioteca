65231156c090405d6ce662193f6e9b476ac291dc
JorgeCDev <jact64@hotmail.com>
2020-10-23 02:46:55 -0600

    Merge origin/master
    Conflicts:
    	src/appbiblioteca/persistencia/ManejaLista.java

---
+++ src/recursos/JPanelGradient.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursos;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jorge
+ */
+public class JPanelGradient extends JPanel {
+    private final Color COLOR_MEDIO = new Color(0, 176, 255);
+    private final Color COLOR_LIGHT = new Color(105, 226, 255);
+    private final Color COLOR_DARK = new Color(0, 129, 203);
+    public JPanelGradient() {
+    }
+    
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        int w = getWidth();
+        int h = getHeight();
+        Color color1 = COLOR_LIGHT ;
+        Color color2 = COLOR_MEDIO;
+        GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, w, h);
+    }
+    
+    
+}
